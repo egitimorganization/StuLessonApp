e49b3434797e4b4c8594947e496d1a4316786b7e
ubeydullahmusab <ubeydullahguneri@hotmail.com>
2016-10-17 10:25:44 +0300

    Add files via upload

---
+++ Ogrenci.java
@@ -0,0 +1,22 @@
+import lombok.Getter;
+import lombok.Setter;
+
+public class Ogrenci {
+
+    @Getter
+    @Setter
+    private static String ogrencino;
+    @Getter
+    @Setter
+    private static String ogrenciadi;
+    @Getter
+    @Setter
+    private static String ogrencisoyadi;
+    @Getter
+    @Setter
+    private static String ogrencisinif;
+    @Getter
+    @Setter
+    private static String ogrencibolum;
+
+}
